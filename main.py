ea4e30fc0cb7c517efc05886096c993eec466a01
talzoor <talzoorjunk@gmail.com>
2018-06-29 14:54:16 +0300

    'Pycharm commit'

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ db = SqliteDatabase('{}/Readings.db'.format(script_path)
 
 
 class PulseData(Model):
-    #  can_delete = True
+    #  scan_delete = True
     Date = DateField()
     Time = TimeField()
     Pulses = IntegerField()
