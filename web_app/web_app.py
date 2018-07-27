3e8c290a6c4521f7330c0f9a39f11aa25903edb8
talzoor <talzoorjunk@gmail.com>
2018-07-27 22:46:06 +0300

    'web app update'

--- web_app/web_app.py
+++ web_app/web_app.py
@@ -1,9 +1,11 @@
 from flask import Flask
-from flask_admin import Admin
-from flask_peewee.admin import Admin
+#from flask_admin import Admin
+#from flask_peewee.admin import Admin
 from flask_peewee.auth import Auth
 from flask_peewee.db import Database
 from flask_admin.contrib.peewee import ModelView
+from flask_peewee.auth import Auth
+
 
 #from flask_admin.contrib.peewee import tools
 import main
@@ -19,17 +21,18 @@ print('db:{}'.format(main.db.database))
 app = Flask(__name__)
 app.config.from_object('config.Configuration')
 db = Database(app)
-database = db.database
+# database = db.database
 
-#auth = Auth(app, db)
+# create an Auth object for use with our flask app and database wrapper
+auth = Auth(app, db)
 
-admin = Admin(app, name='HydroPi_test', template_mode='bootstrap3')
+#admin = Admin(app, name='HydroPi_test', template_mode='bootstrap3')
 #admin = Admin(app, name='HydroPi_test', template_mode='bootstrap3', url='/')
 #admin = Admin(app, name='HydroPi_test', index_view=AdminIndexView(main.PulseData, url='/', endpoint=''))
 
-admin.register(main.PulseData)
+#admin.register(main.PulseData)
 
 #admin.add_view(ModelView(main.PulseData))
-admin.setup()
+#admin.setup()
 
 # Add administrative views here
